a2f156099e4c136fbc0407121340a1f852d6c2ed
Nick Collings <nick@collingo.com>
2014-12-09 19:38:51 +0000

    Minimally reduce GC pressure

--- src/walker-dom.js
+++ src/walker-dom.js
@@ -2,15 +2,13 @@ function WalkerDom() {};
 WalkerDom.prototype = {
   constructor: WalkerDom,
   child: function(cb, node) {
-    var next = node.firstChild;
-    if(next) {
-      cb(next);
+    if(node.firstChild) {
+      cb(node.firstChild);
     }
   },
   sibling: function(cb, node) {
-    var next = node.nextSibling;
-    if(next) {
-      cb(next);
+    if(node.nextSibling) {
+      cb(node.nextSibling);
     }
   }
 };
